451550d474048f7e9fe9f076a98dd8329104ec3e
Kolten Bergeron <koltenb@tamu.edu>
2019-06-30 12:28:15 -0500

    Add xlm_wallet_reader.py

---
+++ xlm_wallet_reader.py
@@ -0,0 +1,33 @@
+from stellar_base.address import Address
+import requests
+import json
+
+# Get data market data from Stellar ticker
+r = requests.get('https://ticker.stellar.org/markets.json')
+json_obj = r.json()
+
+# Parse through market data for USD_XLM pair - has most volume
+def get_price(json_obj, name):
+    for dict in json_obj['pairs']:
+              if dict['name'] == name:
+                  return dict['price']
+
+# Set XLM price in USD ( $1 / # XLM ) 
+xlm_price =  1 / get_price(json_obj, 'XLM_USD') 
+
+# Start 
+print ()
+pub_key = input("Import wallet: ")
+
+# Store address data in address variable
+address = Address(address=pub_key, secret=None, network='public')
+
+# Get information from Horizon
+address.get()
+
+# Store XLM balance and convert to float 
+xlm_balance = float(address.balances[-1]['balance'])
+
+#Print XLM balance and USD equivalent 
+print('\nCurrent XLM Balance: ' + str(xlm_balance) + ' XLM' + ' (~$' + str(xlm_price*xlm_balance) + ')')
+
